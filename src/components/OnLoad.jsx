5cb7ebf6f8a2eeacb665b815f8e0cb1bdb331169
JakeHumphrey1707 <jakematthewh@gmail.com>
2023-07-09 21:30:28 +1000

    OnLoad 0.2

--- src/components/OnLoad.jsx
+++ src/components/OnLoad.jsx
@@ -3,39 +3,46 @@ import React, { useEffect, useState } from 'react';
 
 const OnLoad = () => {
   const [fadeIn, setFadeIn] = useState(true);
-  const [displayText, setDisplayText] = useState(
-    <div className='text-white text-4xl'>
-      Welcome to my portfolio.
-    </div>
-  );
+  const [fadeOut, setFadeOut] = useState(false);
+  const [displayText, setDisplayText] = useState(null);
 
   useEffect(() => {
     // Add a delay before triggering the fade-out effect
     const timeout = setTimeout(() => {
       setFadeIn(false);
+    }, 3000); // Adjust the delay as needed (in milliseconds)
+
+    return () => clearTimeout(timeout);
+  }, [fadeOut]);
+
+  useEffect(() => {
+    if (!fadeIn && !fadeOut) {
       setDisplayText(
-        <div className=''>
+        <div>
           <LucideArrowDown className='h-10 w-10 animate-bounce text-gray-600 flex' />
           <div
-            className='duration-200 cursor-pointer hover:text-gray-600 rounded-md text-gray-300'
-            onClick={() => {
-              // Handle click event here
-            }}
+            className={`duration-200 cursor-pointer hover:text-gray-600 rounded-md text-gray-300 ${fadeOut ? 'fade-out' : ''}`}
+            onClick={() => setFadeOut(true)}
           >
             Take me there.
           </div>
         </div>
       );
-    }, 3000); // Adjust the delay as needed (in milliseconds)
-
-    return () => clearTimeout(timeout);
-  }, []);
+    }
+  }, [fadeIn, fadeOut]);
 
   return (
     <div className='w-screen h-screen bg-black z-50 fixed top-0 left-0'>
       <div className='w-full h-full p-6 justify-center flex items-center'>
-        <div className={`p-1 text-center text-xl ${fadeIn ? 'fade-in-out' : 'fade-in'}`}>
-          {displayText}
+        <div
+          className={`p-1 text-center text-xl ${fadeIn ? 'fade-in-out' : (fadeOut ? 'fade-out' : 'fade-in')}`}
+        >
+          {fadeIn && (
+            <div className='text-white text-4xl'>
+              Welcome to my portfolio.
+            </div>
+          )}
+          {!fadeIn && displayText}
         </div>
       </div>
     </div>
